6d93db14666907cdfb2fc2d351dde2be0514525e
zzzdks760 <zzzdks760@naver.com>
2023-01-19 20:27:57 +0900

    연결설정

--- backend/src/main/java/com/example/blog/config/WebConfig.java
+++ backend/src/main/java/com/example/blog/config/WebConfig.java
@@ -1,4 +1,16 @@
 package com.example.blog.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings (CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8787")
+                .allowedMethods("*")
+                .maxAge(3000);
+    }
 }
--- backend/src/main/java/com/example/blog/controller/TodoController.java
+++ backend/src/main/java/com/example/blog/controller/TodoController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-
+@RequestMapping("/api")
 public class TodoController {
 
     private final TodoService todoService;
